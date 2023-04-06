88ae2d1812ad5d74674de9930d83e4a4aba53b69
cotrade001 <officialvimal01@gmail.com>
2023-04-06 23:10:53 +0530

    first commit

---
+++ assignment.java
@@ -0,0 +1,20 @@
+public class assignment {
+    public static void main(String[] args){
+        int p = 50;
+        int q;
+       q = p; // 
+    System.out.println(q);
+        
+        p += q; // p = p+q = 50 + 
+        System.out.println(p);
+
+        p -= q; // p = p-q = 100-50 = 50 
+        System.out.println(p);
+
+        p /= q; // 1 
+        System.out.println(p);
+        p %= q; // 1
+        System.out.println(p);
+    }
+    
+}
